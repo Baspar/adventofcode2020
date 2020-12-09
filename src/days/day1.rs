2e2191e968f99601d39caa968cd1c9d033f737b6
Baspar <me@bastien-laine.io>
2020-12-09 16:26:19 +0100

    [add] Day1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,16 +1,37 @@
 // Helper
-fn read_input (_input: &str) -> i32 {
-    0
+fn read_input (input: &str) -> Vec<u64> {
+    input
+        .lines()
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect()
 }
 
 // Part1
 pub fn part1 (input: &str) -> String {
-    format!("{}", read_input(input))
+    let nums = read_input(input);
+    for n1 in &nums {
+        for n2 in &nums {
+            if n1 + n2 == 2020 {
+                return format!("{}", n1 * n2)
+            }
+        }
+    }
+    return String::from("")
 }
 
 // Part2
 pub fn part2 (input: &str) -> String {
-    format!("{}", read_input(input))
+    let nums = read_input(input);
+    for n1 in &nums {
+        for n2 in &nums {
+            for n3 in &nums {
+                if n1 + n2 + n3 == 2020 {
+                    return format!("{}", n1 * n2 * n3)
+                }
+            }
+        }
+    }
+    return String::from("")
 }
 
 // Tests
@@ -18,11 +39,23 @@ pub fn part2 (input: &str) -> String {
 mod tests {
     #[test]
     fn day1_part1 () {
-        assert_eq!(super::part1("0"), "0");
+        let input = "1721
+979
+366
+299
+675
+1456";
+        assert_eq!(super::part1(input), "514579");
     }
 
     #[test]
     fn day1_part2 () {
-        assert_eq!(super::part2("0"), "0");
+        let input = "1721
+979
+366
+299
+675
+1456";
+        assert_eq!(super::part2(input), "241861950");
     }
 }
