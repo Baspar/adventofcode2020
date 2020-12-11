630e65e7768deb6fdef846fd4fcbf52f5f2d8f45
Baspar <me@bastien-laine.io>
2020-12-11 12:34:15 +0100

    [add] Day5

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,24 +1,61 @@
+use std::collections::HashSet;
+
 // Helper
-fn read_input (_input: &str) -> i32 {
-    0
+fn read_input (input: &str) -> Vec<Vec<bool>> {
+    input
+        .lines()
+        .map(|line| line
+            .chars()
+            .map(|c| c == 'R' || c == 'B')
+            .collect()
+        )
+        .collect()
 }
 
 // Part1
 pub fn part1 (input: &str) -> String {
-    format!("{}", read_input(input))
+    let instructions = read_input(input);
+    let max_seat = instructions
+        .iter()
+        .map(|instr| instr
+            .iter()
+            .fold(0, |acc, &bit| {
+                2 * acc + if bit {1} else {0}
+            }))
+        .max()
+        .unwrap();
+    format!("{}", max_seat)
 }
 
 // Part2
 pub fn part2 (input: &str) -> String {
-    format!("{}", read_input(input))
+    let instructions = read_input(input);
+    let seats: HashSet<usize> = instructions
+        .iter()
+        .map(|instr| instr
+            .iter()
+            .fold(0, |acc, &bit| {
+                2 * acc + if bit {1} else {0}
+            }))
+        .collect();
+    for seat in 0..1024 {
+        if !seats.contains(&(seat as usize)) {
+            println!("{}", seat);
+        }
+    }
+    format!("{}", 0)
 }
 
 // Tests
 #[cfg(test)]
 mod tests {
+    const INPUT: &str = "BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL";
+
     #[test]
     fn day5_part1 () {
-        assert_eq!(super::part1("0"), "0");
+        assert_eq!(super::part1(INPUT), "820");
     }
 
     #[test]
